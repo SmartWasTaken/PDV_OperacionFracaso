37a751d7b14be65ae023e22451dc484e05de7d57
SmartWasTaken <ineutroniy@gmail.com>
2024-05-07 16:10:03 +0200

    faltan unos ultimos retoques
    Que se vea el nombre del personaje y alguna tonteria mas

--- Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -1,42 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DialogueTrigger : MonoBehaviour
 {
-    [Header("Visual Cue")]
     [SerializeField] private GameObject visualCue;
-
-    [Header("Ink JSON")]
+    [SerializeField] private GameObject visualCue2;
     [SerializeField] private TextAsset inkJSON;
 
     private bool playerInRange;
+    private int interactionsCount;
 
     private void Awake()
     {
         playerInRange = false;
         visualCue.SetActive(false);
+        visualCue2.SetActive(false);
+
+        interactionsCount = 0;
+
+        PlayerPrefs.DeleteKey(gameObject.name + "_InteractionsCount");
+
+        interactionsCount = PlayerPrefs.GetInt(gameObject.name + "_InteractionsCount", 0);
     }
 
     private void Update()
     {
-        if (playerInRange && !DialogoManager.GetInstance().dialogueIsPlaying) 
+        if (playerInRange && !DialogoManager.GetInstance().dialogueIsPlaying)
         {
             visualCue.SetActive(true);
+            visualCue2.SetActive(true);
+
             if (Input.GetKeyDown(KeyCode.E))
             {
-                DialogoManager.GetInstance().EnterDialogueMode(inkJSON);
+                interactionsCount++;
+
+                if (interactionsCount >= 3)
+                {
+                    WinGame();
+                }
+
+                PlayerPrefs.SetInt(gameObject.name + "_InteractionsCount", interactionsCount);
+                PlayerPrefs.Save();
             }
         }
         else
         {
             visualCue.SetActive(false);
+            visualCue2.SetActive(false);
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.CompareTag("Player"))
         {
             playerInRange = true;
         }
@@ -44,9 +62,23 @@ public class DialogueTrigger : MonoBehaviour
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.CompareTag("Player"))
         {
             playerInRange = false;
         }
     }
+
+    private void WinGame()
+    {
+        interactionsCount = 0;
+        PlayerPrefs.DeleteKey(gameObject.name + "_InteractionsCount");
+        PlayerPrefs.Save();
+
+        SceneManager.LoadScene("creditos");
+    }
 }
+
+
+
+
+
